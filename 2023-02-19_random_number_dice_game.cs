5e7cdb4397b423b15827c00c60f88637476d4064
Hamza Tanveer <ha.mza@hotmail.com>
2023-02-19 11:19:37 -0600

    logic implemented for rest of the game

--- 2023-02-19_random_number_dice_game.cs
+++ 2023-02-19_random_number_dice_game.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, random!");
+// If any two dice you roll result in the same value, you get two bonus points for rolling doubles.
+// If all three dice you roll result in the same value, you get six bonus points for rolling triples.
+// If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.
+// 
 
 Random dice = new Random();
 
@@ -9,4 +12,29 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} ");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled Tripples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total > 14)
+{
+    Console.WriteLine("You Win!!!");
+}
+
+if (total < 14)
+{
+    Console.WriteLine("Sorry, You Lose.");
+}
+
+Console.WriteLine(total);
+
