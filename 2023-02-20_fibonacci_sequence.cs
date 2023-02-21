a31b8f04cfaff8146cc7efb9621f43c61f9370f5
hamza10vr <48808837+hamza10vr@users.noreply.github.com>
2023-02-20 21:19:19 -0600

    Fibonacci sequence using List<int>

---
+++ 2023-02-20_fibonacci_sequence.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+var fibonacciNumbers = new List<int> { 1, 1 };
+
+while (fibonacciNumbers.Count < 20)
+{
+    int previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    int previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+}
+
+foreach (var fibonacciNumber in fibonacciNumbers)
+{
+    Console.WriteLine(fibonacciNumber);
+
+}
+
